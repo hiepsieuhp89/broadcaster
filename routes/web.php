f16f701d114b081e9c688af097a11d15372af8f6
Tung <tungcan2000@gmail.com>
2021-09-06 22:28:30 +0700

    bt

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,25 @@ use Illuminate\Support\Facades\Route;
 
 //Route::get('/', [\App\Http\Controllers\Broadcaster\IndexController::class, 'index']);
 
-Route::group(['middleware'=>'auth'],function($route){
+// Route::group(['middleware'=>'auth'],function($route){
 
-  $route->get('/', [\App\Http\Controllers\Broadcaster\IndexController::class, 'index'])
-  ->name('index.navigation.status');
+//   $route->get('/', [\App\Http\Controllers\Broadcaster\IndexController::class, 'index'])
+//   ->name('index.navigation.status');
 
-  $route->get('/devices', [\App\Http\Controllers\Broadcaster\IndexController::class, 'devices'])
-  ->name('index.navigation.devices');
+//   $route->get('/devices', [\App\Http\Controllers\Broadcaster\IndexController::class, 'devices'])
+//   ->name('index.navigation.devices');
 
-  $route->get('/analyze', [\App\Http\Controllers\Broadcaster\IndexController::class, 'analyze'])
-  ->name('index.navigation.analyze');
+//   $route->get('/analyze', [\App\Http\Controllers\Broadcaster\IndexController::class, 'analyze'])
+//   ->name('index.navigation.analyze');
 
-  $route->get('/logout', [\App\Http\Controllers\Broadcaster\Auth\LoginController::class, 'logout'])
-  ->name('auth.logout');
+//   $route->get('/logout', [\App\Http\Controllers\Broadcaster\Auth\LoginController::class, 'logout'])
+//   ->name('auth.logout');
 
-});
+// });
 
 
-//auth login
-Route::get('/login', [\App\Http\Controllers\Broadcaster\Auth\LoginController::class, 'index'])->name('auth.login.index');
-Route::post('/login', [\App\Http\Controllers\Broadcaster\Auth\LoginController::class, 'post'])->name('auth.login.post');
+// //auth login
+// Route::get('/login', [\App\Http\Controllers\Broadcaster\Auth\LoginController::class, 'index'])->name('auth.login.index');
+// Route::post('/login', [\App\Http\Controllers\Broadcaster\Auth\LoginController::class, 'post'])->name('auth.login.post');
 
 
